432446b3b244ef479b8d07e0181dd1363b4965d6
vfeofanov <vasya3000-95@mail.ru>
2025-08-21 17:28:03 +0200

    modified DDPtrain to support mantis==0.2.0

--- Models/Mantis/DDPtrain.py
+++ Models/Mantis/DDPtrain.py
@@ -1,103 +1,89 @@
-import os
+import argparse
 import random
-import numpy as np
 import torch
-import torch.distributed as dist
-import torch.nn.functional as F
+import os
 
+import numpy as np
 import pyarrow as pa
 import pyarrow.dataset as ds
+import torch.nn.functional as F
+import torch.distributed as dist
 
-from mantis_dev.architecture import Mantis8M
-from mantis_dev.trainer import MantisTrainer
+from sklearn.ensemble import RandomForestClassifier
+from importlib.metadata import version, PackageNotFoundError
+from packaging import version as v
 
-def main():
-    ###############################################################################
-    ###############################################################################
+from mantis.architecture import Mantis8M
+from mantis.trainer import MantisTrainer
+
+
+def check_installed_version(package_name, threshold):
+    try:
+        installed_version = version(package_name)
+        if v.parse(installed_version) < v.parse(threshold):
+            raise RuntimeError(
+                f"{package_name} version {threshold} or higher required, "
+                f"but {installed_version} is installed."
+            )
+        else:
+            print(f"{package_name} {installed_version} is OK")
+    except PackageNotFoundError:
+        raise RuntimeError(f"{package_name} is not installed.")
+
+
+def main(cauker_data_path, seed, file_name):
+    # ========= Initialize Distributed Environment =========
     if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
         rank = int(os.environ["RANK"])
         world_size = int(os.environ["WORLD_SIZE"])
         local_rank = int(os.environ.get("LOCAL_RANK", 0))
+        # NCCL backend for distributed initialization
         dist.init_process_group(backend="nccl", init_method="env://")
     else:
         rank = 0
         world_size = 1
         local_rank = 0
 
-    ###############################################################################
-    ###############################################################################
-    SEED = 42
-    random.seed(SEED)
-    np.random.seed(SEED)
-    torch.manual_seed(SEED)
-    torch.cuda.manual_seed_all(SEED)
+    # ========= Set Random Seed =========
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-    ###############################################################################
-    ###############################################################################
+    # ========= Set Device =========
+    # each process corresponds to one GPU, use local_rank to specify which GPU to use for the current process
     device = torch.device(f"cuda:{local_rank}" if torch.cuda.is_available() else "cpu")
 
-    ###############################################################################
-    ###############################################################################
-    arrow_file_path = "/home/sxie/Curriculum/CauKer100K_Kernel6_Parents5.arrow"  
-    dataset_arrow = ds.dataset(source=arrow_file_path, format="arrow")
+    # ========= Check Version of mantis =========
+    check_installed_version(package_name="mantis", threshold="0.2.0")
 
+    # ========= Load Data =========
+    dataset_arrow = ds.dataset(source=cauker_data_path, format="arrow")
     num_total_rows = dataset_arrow.count_rows()
     if rank == 0:
-        print(f"[INFO] Total rows in arrow: {num_total_rows}")
+        print(f"Total rows in arrow: {num_total_rows}")
 
-    # Randomly select 100,000 rows
+    # randomly select 100,000 rows
     num_select = min(100000, num_total_rows)
     indices = random.sample(range(num_total_rows), num_select)
     indices.sort()
-
     table = dataset_arrow.take(indices)
     df_selected = table.to_pandas()  
 
-    # # if you want to load the entire dataset instead of a subset, uncomment the following lines:
+    # if you want to load the entire dataset instead of a subset, uncomment the following lines:
     # table = dataset_arrow.to_table()  
     # df_selected = table.to_pandas()  
 
-
-    ts_list = df_selected["target"].to_list()  
-    X_data = np.stack(ts_list, axis=0)         
-
-    # mask_good = ~np.isnan(X_data).any(axis=1)  
-    # X_data = X_data[mask_good]  
-    # print(f"[INFO] Removed {len(ts_list)-len(X_data)} bad series; remaining {len(X_data)}")
-
-    
-    X_data_torch = torch.tensor(X_data, dtype=torch.float)
-
-    # x = X_data_torch  # [N, 1, 512] float32
-    # print(torch.isnan(x).any(), torch.isinf(x).any())
-    # print(x.min().item(), x.max().item(), x.std().item())
-    # print("TESTED")
-
-
-    old_len = X_data_torch.shape[-1]
-    target_len = 512
-    X_data_torch = X_data_torch.unsqueeze(1)
-
-    if old_len != target_len:
-        X_data_torch = F.interpolate(
-            X_data_torch,
-            size=target_len,
-            mode='linear',
-            align_corners=False
-        )
-        if rank == 0:
-            print(f"[INFO] Interpolated from length {old_len} to {target_len}.")
+    ts_list = df_selected["target"].to_list()
+    X_data = np.stack(ts_list, axis=0)
 
     if rank == 0:
-        print(f"[INFO] Final shape for training data: {X_data_torch.shape}")
-        
+        print("Pretraining data dims: ", X_data.shape)
 
-    ###############################################################################
-    # Trainer
-    ###############################################################################
-    mantis_model = Mantis8M(
+    # ========= Initialize Mantis Model =========
+    network = Mantis8M(
         seq_len=512,
         hidden_dim=256,
         num_patches=32,
@@ -110,37 +96,45 @@ def main():
         transf_dim_head=128,
         transf_dropout=0.1,
         device=device,
-        pre_training=False
+        pre_training=False # when this argument is True, a projection head for contrastive learning is used at the forward step.
+        # we found that without this projector the performance is comparable.
     )
 
+    trainer = MantisTrainer(device=device, network=network)
 
-    trainer = MantisTrainer(device=device, network=mantis_model)
-    # Load a pretrained model if needed
-    #trainer.load("/home/sxie/scaling_tsfm/checkpoint/mean_kernel100K/last_epoch.pth")
-
-    ###############################################################################
-    # Self-supervised pretraining
-    ###############################################################################
+    # ========= Pre-training =========
     if rank == 0:
-        print("[INFO] Starting self-supervised pretraining ...")
-
-    X_for_trainer = X_data_torch.cpu().numpy()
+        print("Starting to pre-train")
+        # if file_name is not None, create absent folders in the path
+        if file_name is not None:
+            dir_path = os.path.dirname(file_name)
+            if dir_path != "":
+                os.makedirs(dir_path, exist_ok=True)
 
     trainer.pretrain(
-        x=X_for_trainer,
-        num_epochs=100,
-        batch_size=256,
-        learning_rate=2e-3,
-        crop_rate_range=[0, 0.2],
-        temperature=0.1,
-        data_parallel=True,
-        checkpoint_path='./checkpoint/',
-        experiment_name='Graph100K-k6P5',
+        X_data,
+        num_epochs=100,                     # Adjust the number of epochs as needed
+        batch_size=256,                     # Adjust batch size based on GPU memory
+        base_learning_rate=2e-3,            # Initial learning rate
+        data_parallel=True,                 # Enable distributed data parallelism
+        learning_rate_adjusting=True,       # Cosine annealing
+        file_name=file_name                 # Where to save the final checkpoint
     )
+    # if the checkpoint was saved, you can load it later by ``trainer.load('file_name')``
 
     if rank == 0:
-        print("[INFO] Pretraining completed! Checkpoints saved in ./checkpoint/ directory.")
+        print("Pre-training is finished")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cauker_data_path', type=str, help='Where generated CauKer data is located.')
+    parser.add_argument('--seed', type=int, default=42, help="Random seed for reproducibility of a single experiment. Note that it is reproducible only on the same machine.")
+    parser.add_argument('--file_name', type=str, default='./checkpoint/Graph100K-k6P5', help="Where to save the final checkpoint. By default, the checkpoint is not saved anywhere.")
+    
+    args = parser.parse_args()
+    cauker_data_path = args.cauker_data_path
+    seed = args.seed
+    file_name = args.file_name
+
+    main(cauker_data_path, seed, file_name)
